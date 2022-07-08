a67d1a018da808f2d94f63731fbe1f85d7f18917
Sebastian <sebasjm@gmail.com>
2022-07-08 11:47:47 -0300

    paid

--- check/notify_insert_coin_when_paid.sh
+++ check/notify_insert_coin_when_paid.sh
@@ -6,12 +6,15 @@ LAST_RESPONSE=`mktemp /tmp/arcade_check-XXXXXX`
 #ACTIVE_ORDERS=$(find $DIR/orders -maxdepth 1 -type f -name '*.active')
 ACTIVE_ORDER=$@
 
-for order in $ACTIVE_ORDERS; do
+DB=github/arcade/pay/orders
+
+for order in $(ssh mendoza ls $DB); do
 	ORDER_ID=$(basename $order | sed 's/.active$//' )
         STATUS=$(curl https://merchant-backend.taler.ar/orders/$ORDER_ID \
                 -w "%{http_code}"  \
                 -s  \
                 -o $LAST_RESPONSE)
 	echo https://merchant-backend.taler.ar/orders/$ORDER_ID is $STATUS
-	[ $STATUS != "402" ] && mv $order $DIR/orders/.checked-$STATUS-$ORDER_ID
+	[ $STATUS != "402" ] && ssh mendoza mv $DB/$order $DB/.checked-$STATUS-$ORDER_ID
+	[ $STATUS == "200" ] && shattered-pixel-dungeon
 done
